9423d91ae65844f1395cd3eaf7fd75bde3bc8caa
sirnusi <davidsanusi360@gmail.com>
2021-10-15 15:29:39 +0100

    rest commit

--- test_app/models.py
+++ test_app/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=200)
     
+    class Meta:
+        verbose_name_plural = 'Categories'
+    
     def __str__(self):
         return self.name
 class Note(models.Model):
@@ -13,7 +16,7 @@ class Note(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField()
     
     def __str__(self):
