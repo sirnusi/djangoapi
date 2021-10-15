a5012964ca15382758422713f10cd0e6802190c2
sirnusi <davidsanusi360@gmail.com>
2021-10-15 14:52:17 +0100

    rest commit

--- django_api/urls.py
+++ django_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/', include('test_app.api.urls'))
+    path('notes/', include('test_app.api.urls'))
 ]
