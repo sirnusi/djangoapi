a77b80711583cdbf70babab305703dd530768b33
sirnusi <davidsanusi360@gmail.com>
2021-10-22 14:18:19 +0100

    rest commit

--- test_app/models.py
+++ test_app/models.py
@@ -18,7 +18,6 @@ class Note(models.Model):
     time_created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField()
     
     def __str__(self):
         return self.title
--- test_app/views.py
+++ test_app/views.py
@@ -1,8 +1,42 @@
 from .serializers import NoteSerializer
 from .models import Note
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+@api_view(['GET', 'POST'])
 def note_list(request):
-    notes = Note.objects.all()
-    serializer = NoteSerializer(notes)
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def note_detail(request, pk):
+    
+    if request.method == 'GET':
+        notes = Note.objects.get(pk=pk)
+        serializer = NoteSerializer(notes)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        notes = Note.objects.get(pk=pk)
+        serializer = NoteSerializer(notes, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        notes = Note.objects.get(pk=pk)
+        notes.delete()
+        return Response()
