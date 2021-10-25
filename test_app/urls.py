62e4fba0474169fbf24276f68e2299a02fe20be8
sirnusi <davidsanusi360@gmail.com>
2021-10-25 02:25:48 +0100

    rest commit

--- test_app/urls.py
+++ test_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('<int:pk>/', views.WatchDetail.as_view(), name='watch-detail'),
     path('streams/', views.StreamPlatformList.as_view(), name='streamplatform-list'),
     path('streams/<int:pk>/', views.StreamPlatformDetail.as_view(), name='streamplatform-detail'),
+    path('reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', views.ReviewDetail.as_view(), name='review-detail')
     # path('stream/<int:pk>/reviews'), # reviews for a particular movie
     # path('stream/reviews/<int:pk/', views.ReviewList.as_view(), name='review-list'), # access individual review to update, destroy, retrieve
     
