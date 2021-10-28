a14c7fa151c08fd1ab3b4ec2815073a384d7e5c8
sirnusi <davidsanusi360@gmail.com>
2021-10-28 17:39:45 +0100

    rest commit

--- user_app/views.py
+++ user_app/views.py
@@ -1,1 +1,13 @@
 #Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from user_app.serializers import RegistrationSerializer
+
+
+@api_view(['POST'])
+def register_user(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
