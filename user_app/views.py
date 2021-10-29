1b97387423b6ef33890d97358affed545fd21de9
sirnusi <davidsanusi360@gmail.com>
2021-10-29 15:00:13 +0100

    rest commit

--- user_app/views.py
+++ user_app/views.py
@@ -1,10 +1,16 @@
 #Create your views here.
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from user_app.serializers import RegistrationSerializer
 from user_app import models
 
+@api_view(['POST'])
+def logout_view(request):
+    if request.method == 'POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register_user(request):
