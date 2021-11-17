d4ec67ccdd21a39fdb0fc6d339fc8f268cf44376
sirnusi <davidsanusi360@gmail.com>
2021-11-17 20:16:53 +0100

    rest commit

--- test_app/models.py
+++ test_app/models.py
@@ -33,4 +33,4 @@ class Review(models.Model):
     active = models.BooleanField(default=True)
     
     def __str__(self):
-        return f'{self.rating} || {self.watchlist.title}'
+        return f'{self.rating} || {self.watchlist.title} || {str(self.owner)}'
--- test_app/permissions.py
+++ test_app/permissions.py
@@ -13,4 +13,4 @@ class ReviewOwnerOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.owner == request.user or request.user.is_staff
