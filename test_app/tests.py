77d5437018f0363464f4a42623d5d462ea718d83
sirnusi <davidsanusi360@gmail.com>
2021-11-29 15:58:57 +0100

    rest commit

--- test_app/tests.py
+++ test_app/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from . import serializers, models
+from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class StreamPlatformTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='example', password='NewPassword')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    
+    def test_streamplatform_create(self):
+        data = {
+            'name': 'Netflix',
+            'about': '#1 in the World.',
+            'website': 'https://netflix.com'
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
