d4eff4c6c4163d38515b36bb3a2a459b53536362
sirnusi <davidsanusi360@gmail.com>
2021-11-29 15:06:52 +0100

    rest commit

--- user_app/tests.py
+++ user_app/tests.py
@@ -30,3 +30,9 @@ class LoginLogoutTestCase(APITestCase):
         }
         response = self.client.post(reverse('login'), data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_logout(self):
+        self.token = Token.objects.get(user__username='example')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
