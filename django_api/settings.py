578c86972b9d0acc189253f27675ef13542ff87f
sirnusi <davidsanusi360@gmail.com>
2021-11-29 17:51:56 +0100

    rest commit

--- django_api/settings.py
+++ django_api/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
      
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     # 'DEFAULT_THROTTLE_CLASSES': [
     #     'rest_framework.throttling.AnonRateThrottle',
--- test_app/serializers.py
+++ test_app/serializers.py
@@ -1,12 +1,14 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import WatchList, StreamPlatform, Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
+    # watchlist = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Review
+        # fields = "__all__"
         exclude = ['watchlist']
-
 class WatchListSerializer(serializers.ModelSerializer):
     # reviews = ReviewSerializer(many=True, read_only=True)
     platform = serializers.StringRelatedField(read_only=True)
