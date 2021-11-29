135196796a806025f2defbbb0d2d612301ed43ee
sirnusi <davidsanusi360@gmail.com>
2021-11-29 16:41:32 +0100

    rest commit

--- django_api/settings.py
+++ django_api/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
      
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ],
     # 'DEFAULT_THROTTLE_CLASSES': [
     #     'rest_framework.throttling.AnonRateThrottle',
