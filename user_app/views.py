cf00aaa32ce165debdc09ee19f4136f016d411d3
sirnusi <davidsanusi360@gmail.com>
2021-12-01 13:22:21 +0100

    rest commit

--- user_app/views.py
+++ user_app/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from user_app.serializers import RegistrationSerializer
-
+from user_app import models
 
 @api_view(['POST'])
 def logout_view(request):
@@ -16,6 +16,20 @@ def logout_view(request):
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        
+        data={}
+        
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            
+            data['Response'] = 'Registration Successful!'
+            data['username'] = account.username
+            data['email'] = account.email
+            
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            return Response(data)
+            
+        else:
+            return Response(data=serializer.errors)
+        
