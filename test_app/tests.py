387da0b8fa64b7d10d9ffac1b9aef25e55722fd3
sirnusi <davidsanusi360@gmail.com>
2021-12-05 21:29:29 +0100

    rest commit

--- test_app/tests.py
+++ test_app/tests.py
@@ -1,22 +1,83 @@
+from django.http import response
 from . import serializers, models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
+from . import models
 
-# class StreamPlatformTestCase(APITestCase):
+class StreamPlatformTestCase(APITestCase):
     
-#     def setUp(self):
-#         self.user = User.objects.create(username='example', password='NewPassword')
-#         self.token = Token.objects.get(user__username=self.user)
-#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+    def setUp(self):
+        self.user = User.objects.create(username='example', password='NewPassword')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name='Netflix', about='#1 in the World', 
+                                                           website='https://netflix.com')
+        
+    def test_streamplatform_create(self):
+        data = {
+            'name': 'Netflix',
+            'about': '#1 in the World.',
+            'website': 'https://netflix.com'
+        }
+        response = self.client.post(reverse('streamplatform-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
-#     def test_streamplatform_create(self):
-#         data = {
-#             'name': 'Netflix',
-#             'about': '#1 in the World.',
-#             'website': 'https://netflix.com'
-#         }
-#         response = self.client.post(reverse('streamplatform-list'), data)
-#         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_streamplatform_list(self):
+        response = self.client.get(reverse('streamplatform-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_streamplatform_ind(self):
+        response = self.client.get(reverse('streamplatform-detail', args=(self.stream.id, )))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_streamplatform_update(self):
+        response = self.client.put(reverse('streamplatform-detail', args=(self.stream.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+    def test_streamplatform_delete(self):
+        response = self.client.delete(reverse('streamplatform-detail', args=(self.stream.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        
+
+class WatchListTestCase(APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='example', password='Password123')
+        self.token = Token.objects.get(user__username=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.stream = models.StreamPlatform.objects.create(name='ROK', about='Just 9ja', website='https://rok.com')
+        self.watchlist = models.WatchList.objects.create(title='New Guy', storyline='Wonderful Movie!!!',
+                                                        platform=self.stream,
+                                                        active=False)
+    
+    def test_watchlist_create(self):
+        data = {
+            'title':'New Guy',
+            'storyline': 'Wonderful Movie!!!',
+            'platform': self.stream,
+            'active': False,
+        }
+        response = self.client.post(reverse('watch-list'), data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('watch-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_watchlist_detail(self):
+        response = self.client.get(reverse('watch-detail', args=(self.watchlist.id, )))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(models.WatchList.objects.get().title, 'New Guy')
+    
+    def test_watchlist_update(self):
+        response = self.client.put(reverse('watch-detail', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    
+    def test_watchlist_delete(self):
+        response = self.client.delete(reverse('watch-detail', args=(self.watchlist.id, )))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
