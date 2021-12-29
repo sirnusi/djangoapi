a96cbac7bacc52240c93cc7119df1e73e0eb1056
sirnusi <davidsanusi360@gmail.com>
2021-12-29 17:12:18 +0100

    rest commit

--- django_api/settings.py
+++ django_api/settings.py
@@ -152,6 +152,10 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
 }
-# SIMPLE_JWT = {
-#     'ROTATE_REFRESH_TOKENS': True,
-# }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+}
